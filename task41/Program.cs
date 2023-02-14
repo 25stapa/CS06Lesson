373f7030d37a73765299237c6243a9f5cee621da
stapa <stapa@me.com>
2023-02-15 00:27:21 +0300

    init

---
+++ task41/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+
+Console.Write("Enter numbers separated by commas: ");
+string[] strArray = Console.ReadLine().Split(',');
+
+int count = 0;
+
+int[] result = new int[strArray.Length];
+
+for (int i = 0; i < result.Length; i++)
+{
+    result[i] = int.Parse(strArray[i]);
+    if (result[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine($"User entered {count} numbers greather than 0");
